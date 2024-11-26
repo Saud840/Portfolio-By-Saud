59d34df63aa9bd89829247ba47947a2f33f447c4
Saud Zubair <m.saud840@gmail.com>
2024-11-27 01:04:34 +0500

    Add files via upload

---
+++ src/app/navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+function Navbar(){
+    return(
+            <div className="p-3">
+                <div className="flex justify-between">
+                    <Image src="./logo.jpg" alt="logo" className="w-16 md:w-20 lg:w-34 xl:w-38"/>
+                    <nav className="flex items-center justify-between gap-4 md:gap-6 lg:gap-8">
+                        <div className="text-[#020202] font-sans text-lg md:font-sans md:text-2xl lg:font-sans lg:text-3xl hover:text-[#6E06F2]"><Link href="./">Home</Link></div>
+                        <div className="text-[#020202] font-sans text-lg md:font-sans md:text-2xl lg:font-sans lg:text-3xl hover:text-[#6E06F2]"><Link href="./projects">Projects</Link></div>
+                        <div className="text-[#020202] font-sans text-lg md:font-sans md:text-2xl lg:font-sans lg:text-3xl hover:text-[#6E06F2]"><Link href="./about">About</Link></div>
+                        <div className="text-[#020202] font-sans text-lg md:font-sans md:text-2xl lg:font-sans lg:text-3xl hover:text-[#6E06F2]"><Link href="./contact">Contact Us</Link></div>
+                    </nav>
+                    <Image src="./message.jpg" alt="icon" className="w-16 md:w-24 lg:w-34 xl:w-38"/>
+                </div>
+            </div>
+
+);
+}
+
+export default Navbar
